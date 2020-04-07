8e8f74a2fb861b3b7d4bff8f41b3a8e117a75d07
SAKTHISIVANI18 <59279036+SAKTHISIVANI18@users.noreply.github.com>
2020-04-07 09:55:01 +0200

    Add files via upload

---
+++ first.java
@@ -0,0 +1,5 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[Cfirst[B[B[B[B[D[D[D[D[D[D[D[D[D[D
